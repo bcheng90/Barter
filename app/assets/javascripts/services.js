ded7a59da3d02ab3fad5afc9c1e7ca55c98a2651
Ben Cheng & Natalia Bryzhatenko & Ryan <pair+bcheng90.notika314.rimmesbe@devbootcamp.com>
2015-04-18 16:04:45 -0400

    got tabs working with angular on welcome page

--- app/assets/javascripts/services.js
+++ app/assets/javascripts/services.js
@@ -1,3 +1,7 @@
 angular.module("barter").factory("UserService", function($resource){
    return $resource("/users/:id");
 });
+
+angular.module("barter").factory("TalentService", function($resource){
+   return $resource("/talents/:id");
+});
