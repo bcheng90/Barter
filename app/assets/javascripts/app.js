651b894a597300a888de86eda40ccb17972b4129
Steven Cassidy <steven@mumsnet.com>
2015-04-18 23:27:07 -0400

    Enable user editing

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,13 @@
 // change the routes
 angular.module('barter', ['ngNewRouter', 'ngResource']);
+
+//Tell angular to use the CSRF token rails provides on the page
+angular.module('barter').config(function($httpProvider) {
+  $httpProvider.defaults.headers.common['X-CSRF-Token'] =
+    $('meta[name=csrf-token]').attr('content');
+});
+
+
 angular.module('barter').controller('AppController', ['$router', AppController]);
 
 function AppController ($router) {
