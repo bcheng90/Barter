d5b3c5cf020f620f22fa3c8c301f4c3ca46dec04
Natalia Bryzhatenko <notika314@gmail.com>
2015-04-20 16:00:41 -0400

    edit

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -39,19 +39,17 @@ angular.module('barter')
 
 
 }]);
-
 angular.module('barter')
-
-.controller('UsersController', ['$routeParams', 'UserService', "TalentService", function($routeParams, UserService, TalentService){
+.controller('UsersController', ['$http', '$routeParams', 'UserService', function($http, $routeParams, UserService, TalentService, OfferService){
   this.categories = ["Art & Music", "Food", "Sport", "Computer"];
   this.experiences = ["novice", "intermediate", "expert"];
 
   this.loadUserGraph = function() {
      UserService.get({id: $routeParams.id}, function(data){
        this.user = data;
-       console.log("in reload user function");
      }.bind(this));
    }
+
 
    this.loadUserGraph();
 
@@ -81,15 +79,18 @@ angular.module('barter')
   };
 
   this.acceptOffer = function(offer) {
+    var that = this;
     offer.status = true;
     $http.put('/offers/' + offer.id, offer)
     .success(function(data, status){
+      that.loadUserGraph();
       console.log(data);
       console.log(status);
     });
   };
 
   this.declineOffer = function(offer) {
+    var that = this;
     $http.delete('/offers/' + offer.id, offer)
     .success(function(response){
       that.loadUserGraph();
@@ -103,15 +104,43 @@ angular.module('barter')
     });
   };
 
+  function insertOffer(newOffer, user) {
+    var slots = user.timeslots;
+    slots.forEach(function(slot) {
+      if (slot.id === newOffer.timeslot_id) {
+        slot.offers.push(newOffer);
+      }
+    })
+  }
+
+  function removeOffer(offer, user) {
+    var slots = user.timeslots;
+    slots.forEach(function(slot) {
+      if (slot.id === offer.timeslot_id) {
+        var index = slot.offers.indexOf(offer);
+        if (index > -1) {
+            slot.offers.splice(index, 1);
+        }
+      }
+    });
+
+  }
+
+
   this.createOffer = function(timeslot) {
+    var that = this;
     console.log("creating offer");
     $http.post('/offers', timeslot)
     .success(function(response){
+      var newOffer = response;
+      insertOffer(newOffer, that.user);
+      console.log(that.user.timeslots);
       console.log("success!!!");
     }).error(function(response){
       console.log("not in success but getting there!!!");
     });
   };
+
 
   this.convertType = function(type) {
     if (type == "artMusic") {
