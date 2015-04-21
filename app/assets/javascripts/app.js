08df0badfccda178271735ad95f8ef641776569e
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-21 15:40:12 -0400

    Restart s3 process

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -10,24 +10,6 @@ angular.module('barter').config(function($httpProvider) {
 
 angular.module('barter').controller('AppController', ['$router', '$http', AppController]);
 
-angular.module('directives', []).directive('file', function() {
-  return {
-    restrict: 'AE',
-    scope: {
-      file: '@'
-    },
-    link: function(scope, el, attrs){
-      el.bind('change', function(event){
-        var files = event.target.files;
-        var file = files[0];
-        scope.file = file;
-        scope.$parent.file = file;
-        scope.$apply();
-      });
-    }
-  };
-});
-
 function AppController ($router, $http) {
   $router.config([
     {path: '/', component: 'welcome' },
