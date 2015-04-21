1d36123ef2c6d150443ea4a8bc6b55c92e0dc5a6
Notika314 <notika314@gmail.com>
2015-04-21 14:16:31 -0400

    hide create offer btn if user already created offer

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -187,7 +187,6 @@ angular.module('barter')
     $http.post('/offers', timeslot)
     .success(function(response){
       var newOffer = response;
-      insertOffer(newOffer, that.user);
       that.loadUserGraph();
     }).error(function(response){
       console.log("not in success but getting there!!!");
@@ -202,7 +201,25 @@ angular.module('barter')
      }
   };
 
-}]);
+  this.hasOffer = function(timeslot,curusid) {
+    for (var i = 0; i < timeslot.offers.length; i++){
+      if (timeslot.offers[i].student.id == curusid) {
+         return true;
+         console.log("not found yet");
+      };
+    }
+  };
+
+}])
+// .directive('weekDay', function(){
+//   return {
+//     restrict: 'E',
+//     scope: {
+//       winfo : '=info'
+//     },
+//     templateUrl : 'wday-iso.html'
+//   }
+// });
 
 
 
