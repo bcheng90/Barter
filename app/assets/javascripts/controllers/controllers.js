ff9006323e59b221b6931da7e4cd2c816d2b982b
Ben Cheng <benjamin.cheng90@gmail.com>
2015-04-22 15:14:10 -0400

    Begin switching bootstrap to angularbootstrap

--- app/assets/javascripts/controllers/controllers.js
+++ app/assets/javascripts/controllers/controllers.js
@@ -1,3 +1,20 @@
+// TODO: Use angular to replace all bootstrap
+// angular.module('ui.bootstrap.demo').controller('CarouselDemoCtrl', function ($scope) {
+//   $scope.myInterval = 5000;
+//   var slides = $scope.slides = [];
+//   $scope.addSlide = function() {
+//     var newWidth = 600 + slides.length + 1;
+//     slides.push({
+//       image: 'http://placekitten.com/' + newWidth + '/300',
+//       text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+//         ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+//     });
+//   };
+//   for (var i=0; i<4; i++) {
+//     $scope.addSlide();
+//   }
+// });
+
 angular.module('barter')
   .controller('WelcomeController', ['$routeParams', "UserService", "TalentService", function($routeParams, UserService, TalentService ){
     this.tals = [];
